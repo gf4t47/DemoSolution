48aac714f8afc128a20c3c91f1427f17573bd567
kern.ding <gf4t47@gmail.com>
2024-05-27 16:32:40 -0700

    more demo data

--- Src/Domain/Ordering/Ordering.Manager/OrderingHostService.cs
+++ Src/Domain/Ordering/Ordering.Manager/OrderingHostService.cs
@@ -18,11 +18,30 @@ public class OrderingHostService(OrderingCommandBus commandBus) : BackgroundLong
     {
         get
         {
-            var address = new Address("street", "LA", "CA", 91100);
-            var customer = new Customer(1, "Test", address);
-            var dishes = new List<Dishes> { new("Pizza"), new("Noodles")};
-            var order = new VerifyData(customer, dishes, address);
-            yield return new AcceptOrder(order);
+            {
+                var address = new Address("long long bach", "LA", "CA", 91100);
+                var customer = new Customer(1, "Test1", address);
+                var dishes = new List<Dishes> { new("Pizza"), new("Noodles")};
+                var order = new VerifyData(customer, dishes, address);
+                yield return new AcceptOrder(order);
+            }
+
+            {
+                var address = new Address("high high sky", "Top", "FL", 93245);
+                var customer = new Customer(2, "Test2", address);
+                var dishes = new List<Dishes> { new("Ramen"), new("Burger")};
+                var order = new VerifyData(customer, dishes, address);
+                yield return new AcceptOrder(order);                
+            }
+
+            {
+                var address = new Address("deep deep sea", "Park", "BL", 12345);
+                var customer = new Customer(3, "Test3", address);
+                var dishes = new List<Dishes> { new("WuDong"), new("XiaoLongBao")};
+                var order = new VerifyData(customer, dishes, address);
+                yield return new AcceptOrder(order);                
+            }
+
         }
     }
 
